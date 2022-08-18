b39b24ea3c9c520282c875be4662eed50de631cc
Vadim Gusak <gusakvd@gmail.com>
2022-08-18 14:17:36 +1000

    add brain even game

---
+++ brain_games/brain_even.py
@@ -0,0 +1,46 @@
+import prompt
+from random import randint
+
+
+START = 1
+END = 999
+NUM_OF_ROUNDS = 3
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+def brain_even():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    wrong_count = 0
+    correct_count = 0
+    correct_anser = ''
+
+    while wrong_count == 0 and correct_count < NUM_OF_ROUNDS:
+        random_num = randint(START, END)
+        print(f'Question: {random_num}')
+        user_anser = prompt.string('Your answer: ')
+        if (is_even(random_num) and user_anser == 'yes'):
+            print('Correct!')
+            correct_count += 1
+        elif not is_even(random_num) and user_anser == 'no':
+            print('Correct!')
+            correct_count += 1
+        else:
+            if is_even(random_num):
+                correct_anser = 'yes'
+            else:
+                correct_anser = 'no'
+            print(f"'{user_anser}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_anser}'.")
+            print(f"Let's try again, {name}!")
+            wrong_count += 1
+
+    if wrong_count == 0:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from brain_games.brain_even import brain_even
+
+
+def greeting():
+    print('Welcome to the Brain Games!')
+
+
+def main():
+    greeting()
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
