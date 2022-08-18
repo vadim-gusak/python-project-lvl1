cf5b3903e7d767d3899e15f1e06d2b02bbda991f
Vadim Gusak <gusakvd@gmail.com>
2022-08-18 10:41:59 +1000

    add welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+from brain_games.scripts.cli import welcome_user
 
 def greeting():
     print('Welcome to the Brain Games!')
 
 def main():
     greeting()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
