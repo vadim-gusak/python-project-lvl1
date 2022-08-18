fcba6425506a40362ad7798581c92be4a6d17450
Vadim Gusak <gusakvd@gmail.com>
2022-08-18 11:59:56 +1000

    fix flake8 remarks in brain_games, cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from brain_games.scripts.cli import welcome_user
 
+
 def greeting():
     print('Welcome to the Brain Games!')
+
 
 def main():
     greeting()
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
