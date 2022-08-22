9c8b9fbfd7427208a4c9de279425f7553edc5e19
Vadim Gusak <gusakvd@gmail.com>
2022-08-22 13:12:02 +1000

    add game_logic file

---
+++ brain_games/game_logic.py
@@ -0,0 +1,36 @@
+import prompt
+
+
+NUM_OF_ROUNDS = 3
+
+
+def game(game_module):
+    user_name = greeting()
+    game_module.task()
+    wrong = 0
+    win = 0
+    while wrong == 0 and win < NUM_OF_ROUNDS:
+        correct_anser = str(game_module.main())
+        user_anser = prompt.string('Your answer: ')
+        if user_anser == correct_anser:
+            win += 1
+            print('Correct!')
+        else:
+            wrong += 1
+    end(user_name, wrong, user_anser, correct_anser)
+
+
+def greeting():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    return user_name
+
+
+def end(user_name, wrong_count, user_anser, correct_anser):
+    if wrong_count == 0:
+        print(f'Congratulations, {user_name}!')
+    else:
+        print(f"'{user_anser}' is wrong answer ;(. "
+        f"Correct answer was '{correct_anser}'.")
+        print(f"Let's try again, {user_name}!")
