c28f28c22ecd239a155eb5d71343b3bef07e76a1
Vadim Gusak <gusakvd@gmail.com>
2022-08-25 13:33:20 +1000

    rewrite game func in brain_progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,26 +5,35 @@ START = 1
 END = 100
 DIFF_START = 1
 DIFF_END = 100
-LENGTH_START = 5
+LENGTH_START = 1
+LENGTH_MIN = 5
 LENGTH_END = 10
 TASK = 'What number is missing in the progression?'
 
 
-def main():
-    sum = randint(START, END)
-    add_num = randint(DIFF_START, DIFF_END)
-    prog_length = randint(LENGTH_START, LENGTH_END)
-    rand_num = randint(LENGTH_START, prog_length)
+def calc_term(initial_term, common_difference, n):
+    term = initial_term + (n - 1) * common_difference
+    return term
+
+
+def create_question(prog_length, start_term, difference, rand_num):
     index = 1
     question = ''
-    anser = ''
     while index <= prog_length:
-        if index != rand_num:
-            question = question + str(sum) + ' '
-            sum = sum + add_num
+        if index == rand_num:
+            question = f'{question} ...'
         else:
-            question = question + '... '
-            anser = sum
-            sum = sum + add_num
+            question = f'{question} {calc_term(start_term, difference, index)}'
         index += 1
-    return (question.strip(), str(anser))
+    return question
+
+
+def question_and_answer():
+    initial_term = randint(START, END)
+    difference = randint(DIFF_START, DIFF_END)
+    prog_length = randint(LENGTH_MIN, LENGTH_END)
+    rand_num = randint(LENGTH_START, prog_length)
+
+    question = create_question(prog_length, initial_term, difference, rand_num)
+    answer = calc_term(initial_term, difference, rand_num)
+    return (question.strip(), str(answer))
